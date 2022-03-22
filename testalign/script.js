7cedd53c5843f962c0b8e2594cde645449dd37bc
Elivelton Ferreira <72666573+Vettoferreira@users.noreply.github.com>
2022-03-21 22:23:36 -0300

    Initial commit

---
+++ testalign/script.js
@@ -0,0 +1,10 @@
+function calcular(){
+
+var tes1 = document.getElementsByName('test1')
+var tes2 = document.getElementsByName('test2')
+var tes3 = document.getElementsByName('test3')
+var cont = document.getElementById('container')
+var img = document.createElement('img')
+
+    
+}
---
+++ testglass/script.js
@@ -0,0 +1,4 @@
+function analisar() {
+
+    window.alert('ok!')
+}
